e1d194568882b399dcdaf5b5c4ad3bd2105fd33a
Suraj Prasad <surajjprasad@gmail.com>
2022-07-07 17:11:17 +0530

    flick-player-plugin added

--- lib/video_page.dart
+++ lib/video_page.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:flick_video_player/flick_video_player.dart';
+
 
 class VideoPage extends StatefulWidget {
   final String filePath;
@@ -13,49 +15,27 @@ class VideoPage extends StatefulWidget {
 }
 
 class _VideoPageState extends State<VideoPage> {
-  late VideoPlayerController _videoPlayerController;
+  late FlickManager flickManager;
+  @override
+  void initState() {
+    super.initState();
+    flickManager = FlickManager(
+      videoPlayerController:
+      VideoPlayerController.file(File(widget.filePath)),
+    );
+  }
 
   @override
   void dispose() {
-    _videoPlayerController.dispose();
+    flickManager.dispose();
     super.dispose();
-  }
-
-  Future _initVideoPlayer() async {
-    _videoPlayerController = VideoPlayerController.file(File(widget.filePath));
-    await _videoPlayerController.initialize();
-    await _videoPlayerController.setLooping(true);
-    await _videoPlayerController.play();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Preview'),
-        elevation: 0,
-        backgroundColor: Colors.black26,
-        actions: [
-          // IconButton(
-          //   icon: const Icon(Icons.check),
-          //   onPressed: () {
-          //     // print(':::::::::::::::::::::::::::::::${widget.filePath}' );
-          //     var file = File(widget.filePath);
-          //     // print(':::::::::::::::::::::::::::::::::::${widget.filePath}:::::${file.lengthSync()}');
-          //   },
-          // )
-        ],
-      ),
-      extendBodyBehindAppBar: true,
-      body: FutureBuilder(
-        future: _initVideoPlayer(),
-        builder: (context, state) {
-          if (state.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            return VideoPlayer(_videoPlayerController);
-          }
-        },
+    return Container(
+      child: FlickVideoPlayer(
+          flickManager: flickManager
       ),
     );
   }
